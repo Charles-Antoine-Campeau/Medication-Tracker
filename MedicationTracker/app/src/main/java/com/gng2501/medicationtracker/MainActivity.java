d396074f8f974d1fef6979cefc109f695c67bd67
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-01-15 19:51:47 -0500

    Creating main layout and class.  Adding info to gradle and manifest

---
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gng2501.medicationtracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
