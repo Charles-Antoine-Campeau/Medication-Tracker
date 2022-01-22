5af6e0fcba50298fa7b0c953a58fce696aac0e94
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-01-22 17:10:15 -0500

    Creating classes associated to layouts

---
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/Alarms.java
@@ -0,0 +1,14 @@
+package com.gng2501.medicationtracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Alarms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarms);
+    }
+}
---
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/MedicationList.java
@@ -0,0 +1,14 @@
+package com.gng2501.medicationtracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MedicationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.medication_list);
+    }
+}
