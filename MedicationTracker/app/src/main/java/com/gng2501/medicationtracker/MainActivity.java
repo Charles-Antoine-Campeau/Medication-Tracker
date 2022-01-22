55972ff1355017c9619953af67392099db364953
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-01-22 18:12:44 -0500

    Implementing buttons on main menu and adding new activity to manifest

--- MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/MainActivity.java
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.gng2501.medicationtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        Button btnAlarms = findViewById(R.id.btnAlarms);
+        Button btnMedicationList = findViewById(R.id.btnMedicationList);
+        Button btnSettings = findViewById(R.id.btnSettings);
+
+        btnAlarms.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), Alarms.class));
+        });
+
+        btnMedicationList.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MedicationList.class));
+        });
+
+        btnSettings.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), Settings.class));
+        });
     }
+
+
 }
