6a4f0f2325a2e209d51f6bb445b771b02736b36f
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-01-30 10:00:35 -0500

    creating new layout to add new medication

---
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/layoutcode/CreateMedication.java
@@ -0,0 +1,16 @@
+package com.gng2501.medicationtracker.layoutcode;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gng2501.medicationtracker.R;
+
+public class CreateMedication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_medication);
+    }
+}
