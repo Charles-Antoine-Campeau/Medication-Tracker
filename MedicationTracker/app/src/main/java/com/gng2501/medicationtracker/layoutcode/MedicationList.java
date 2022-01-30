0f33e9942cda062165736c4e2346a23fa72c9c0d
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-01-30 11:03:39 -0500

    implementing onCreate for MedicationList class

--- MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/layoutcode/MedicationList.java
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/layoutcode/MedicationList.java
@@ -1,6 +1,8 @@
 package com.gng2501.medicationtracker.layoutcode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,11 @@ public class MedicationList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.medication_list);
+
+        Button btnNewMedication = findViewById(R.id.btnAddNewMedication);
+
+        btnNewMedication.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), CreateMedication.class));
+        });
     }
 }
