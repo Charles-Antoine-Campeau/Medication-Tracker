0a94a78c7d52c35547b46d7bc726b7236ff6eecd
Charles-Antoine-Campeau <campeauca@gmail.com>
2022-02-05 17:08:18 -0500

    Registering EditMedication class as activity

--- MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/layoutcode/EditMedication.java
+++ MedicationTracker/app/src/main/java/com/gng2501/medicationtracker/layoutcode/EditMedication.java
@@ -1,4 +1,16 @@
 package com.gng2501.medicationtracker.layoutcode;
 
-public class EditMedication {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gng2501.medicationtracker.R;
+
+public class EditMedication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_medication);
+    }
 }
